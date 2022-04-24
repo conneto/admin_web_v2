f9b9972a2e60c128c59b7e6ca9a64d00bdf615b4
kangkang2410 <haphongpk12@gmail.com>
2022-04-24 13:25:22 +0700

    Update

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/adminWebV2'),
+      dir: require('path').join(__dirname, './coverage/admin_web_v2'),
       subdir: '.',
       reporters: [
         { type: 'html' },
