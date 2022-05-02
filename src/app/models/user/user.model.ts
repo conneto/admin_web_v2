f14fa63101fb2aba794f86e799ada6e681c1424b
kangkang2410 <haphongpk12@gmail.com>
2022-05-02 15:26:55 +0700

    Update

--- src/app/models/user/user.model.ts
+++ src/app/models/user/user.model.ts
@@ -1,27 +1,57 @@
-import { Injectable } from "@angular/core";
-import { Adapter } from "src/app/interface/adapter";
+import { Injectable } from '@angular/core';
+import { Adapter } from 'src/app/interface/adapter';
 
 export class User {
-    constructor(public id?: string, public username?: string, public email?: string,
-        public gender?: string, public birthday?: string, public token?: string,
-        public first_name?: string, public last_name?: string, public number_phone?: string,
-        public avatar?: string,
-        public operating_unit?: string, public is_active?: boolean, public is_block?: boolean, public created_at?: number, public updated_at?: number, public role_id?: string,) {
-
-    }
+  constructor(
+    public id?: string,
+    public username?: string,
+    public email?: string,
+    public gender?: string,
+    public birthday?: string,
+    public token?: string,
+    public first_name?: string,
+    public last_name?: string,
+    public number_phone?: string,
+    public avatar?: string,
+    public donated_amount?: number,
+    public donated_turn?: number,
+    public participated_turn?: number,
+    public donated_campaign?: number,
+    public participated_campaign?: number,
+    public is_active?: boolean,
+    public is_block?: boolean,
+    public created_at?: number,
+    public updated_at?: number,
+    public role_id?: string
+  ) {}
 }
 
 @Injectable({
-    providedIn: 'root',
+  providedIn: 'root',
 })
-
-export class UserAdapter implements Adapter<User>{
-    adapt(item: any): User {
-        return new User(item.id, item.username,
-            item.email, item.gender, item.birthday,
-            item.token, item.first_name, item.last_name, item.number_phone, item.avatar, item.operating_unit,
-            item.is_active, item.is_block, item.created_at, item.updated_at, item.role_id,
-        )
-    }
-
+export class UserAdapter implements Adapter<User> {
+  adapt(item: any): User {
+    return new User(
+      item.id,
+      item.username,
+      item.email,
+      item.gender,
+      item.birthday,
+      item.token,
+      item.first_name,
+      item.last_name,
+      item.number_phone,
+      item.avatar,
+      item.donated_amount,
+      item.donated_turn,
+      item.participated_turn,
+      item.donated_campaign,
+      item.participated_campaign,
+      item.is_active,
+      item.is_block,
+      item.created_at,
+      item.updated_at,
+      item.role_id
+    );
+  }
 }
